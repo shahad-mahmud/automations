8c752bdc50105e3cff3a0473d70604f53840f5d6
Shahad Mahmud <shahad9381@gmail.com>
2021-04-18 08:52:22 +0600

    timing change

--- hydrator_automation.py
+++ hydrator_automation.py
@@ -22,20 +22,25 @@ for i in range(start_range, 291, -1):
 
     # get to the file
     pg.moveTo(500, 300)
-    time.sleep(0.2)
+    time.sleep(1.5)
     if i < 310:
         pg.scroll(-24)
-        file_y = 350 + 24 * (309 - i)
-    elif i < 335:
+        time.sleep(1.5)
+        file_y = 360 + 23 * (309 - i)
+    elif i < 334:
         pg.scroll(-16)
-        file_y = 175 + 24 * (334 - i)
+        time.sleep(1.5)
+        file_y = 150 + 23 * (333 - i)
     elif i < 359:
         pg.scroll(-8)
+        time.sleep(1.5)
         file_y = 183 + 24 * (358 - i)
     else:
         file_y = 190 + 24 * (382 - i)
 
     # click on file
+    pg.click(file_x, file_y, 1)
+    time.sleep(1.5)
     pg.click(file_x, file_y, 2)
     time.sleep(1.5)
 
@@ -67,14 +72,14 @@ for i in range(start_range, 291, -1):
     pg.click(1238, 52)
     time.sleep(1.5)
 
-    # sleep for 5 min and pause 
-    time.sleep(300)
+    # sleep for 6 min and pause 
+    time.sleep(360)
     pg.click(496, 314)
 
     # sleep 10 sec and start
     time.sleep(10)
     pg.click(496, 314)
 
-    # sleep for 5 min and start new
-    time.sleep(300)
+    # sleep for 6 min and start new
+    time.sleep(360)
 
